aad6f46a8430e15ab5f97418cc17ad7892b70f8c
AK-Nobleman <axelkuarsani@gmail.com>
2024-09-03 21:22:11 +0700

    CameraMove

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject MainCam;
+    public GameObject SecondCam;
+    public bool CameraON = false;
+    
+    public void CameraMaanager()
     {
-        
+        if(CameraON == true)
+        {
+            Cam1();
+            CameraON = false;
+        }
+        else
+        {
+            Cam2 ();
+            CameraON = true;
+        }
+    }
+
+    void Cam1()
+    {
+        MainCam.SetActive(true);
+        SecondCam.SetActive(false);
+    }
+
+    void Cam2()
+    {
+        MainCam.SetActive(false);
+        SecondCam.SetActive(true);
     }
 
     // Update is called once per frame
